1e2148d2d2cf8d0fa9ec8b1e1326484e83c68225
michele dibenedetto <michele.dibe78@gmail.com>
2020-03-22 15:10:21 +0000

    code enhancement: now is possible to send a video url as param of the application. bug fix: now url with multi params are coreclty parsed;

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-const txtUrl = document.querySelector('#txtUrl');
-const iframe = document.querySelector('#player');
-const btnParseUrl = document.querySelector('#txtUrl');
+const txtUrl = document.querySelector("#txtUrl");
+const iframe = document.querySelector("#player");
+const btnParseUrl = document.querySelector("#txtUrl");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setSourceFromWindowParam();
+  parseYoutubeVideoUrl();
+});
+function setSourceFromWindowParam() {
+  const PARAM_NAME = "videoUrl";
+  const { search } = window.location;
+
+  if (search.includes(PARAM_NAME)) {
+    var REGEX = new RegExp(`(?<=${PARAM_NAME}=).*$`);
+    const videoUrl = search.match(REGEX);
+    txtUrl.value = videoUrl;
+  }
+}
 
 function selectText() {
-	txtUrl.select();
+  txtUrl.select();
 }
 
 function setSource() {
-	const source = txtUrl.value.trim();
-	iframe.setAttribute('src', source);
+  const source = txtUrl.value.trim();
+  iframe.setAttribute("src", source);
 }
 
 function parseYoutubeVideoUrl() {
-	const url = txtUrl.value.trim();
-	const parsedUrl = getYoutubeVideoCode(url);
-	const template = 'https://www.youtube.com/embed/';
-	const source = template + parsedUrl;
+  const url = txtUrl.value.trim();
+  const parsedUrl = getYoutubeVideoCode(url);
+  const template = "https://www.youtube.com/embed/";
+  const source = template + parsedUrl;
 
-	if (!source) {
-		alert(`The url: ${url} cannot be parsed :( `);
-	} else {
-		txtUrl.value = source;
-		iframe.setAttribute('src', source);
-	}
+  if (!source) {
+    alert(`The url: ${url} cannot be parsed :( `);
+  } else {
+    txtUrl.value = source;
+    iframe.setAttribute("src", source);
+  }
 }
 
 async function pasteClipBoard() {
-	selectText();
-	const text = await navigator.clipboard.readText();
-	txtUrl.value = text;
+  selectText();
+  const text = await navigator.clipboard.readText();
+  txtUrl.value = text;
 }
 
-function getYoutubeVideoCode(url) {
-	const REGEX = /(?<=watch\?v=).*(?=[&\s])/;
-	const matches = (url + ' ').match(REGEX);
-	const videoCode = (matches || []).length > 0 ? matches[0] : null;
+function getYoutubeVideoCode(rawUrl) {
+  const url = rawUrl.split("&")[0];
 
-	return videoCode;
+  const REGEX = /(?<=watch\?v=).*(?=[&\s])/;
+  const matches = (url + " ").match(REGEX);
+  const videoCode = (matches || []).length > 0 ? matches[0] : null;
+
+  return videoCode;
 }
 
 /*
